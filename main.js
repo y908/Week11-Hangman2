3c0a3f0633f58d803e21d0fd1fa695f4117c3d91
Yana Yakubchik <Yana Yakubchik>
2016-07-15 11:08:01 -0400

    tested user input

--- main.js
+++ main.js
@@ -5,16 +5,22 @@ var bank = require('./game');
 inquirer.prompt([
 
    {
-    type: "list",
-    name: "selection",
-    message: "Let's play hangman! What level do you want ",
-    choices: ["hard", "medium", "easy",] 
+    type: "input",
+    name: "question",
+    message: "Guess a Letter:",
+     
   }
 
-  ]).then(function (answers) {
+  ]).then(function (answer) {
+    if (answer.question < 64 || answer.question > 91){
+      console.log("this works");
+    }
 
+    else{
+      console.log(bank.wordbank.words[2]);
+    }
 
-  console.log(bank.wordbank.words[2]);
+ 
 
   // Use user feedback for... whatever!! 
 }); 
